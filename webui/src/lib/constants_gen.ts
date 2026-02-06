362e8e01b7efb9f347aa27025e8b028cbfbac9d2
Tools-app <localhost.hutao@gmail.com>
2026-02-06 15:33:31 +0800

    chore: ignore webui/src/lib/constants_gen.ts

--- webui/src/lib/constants_gen.ts
+++ webui/src/lib/constants_gen.ts
@@ -1,9 +1,5 @@
-/**
- * Copyright 2025 Meta-Hybrid Mount Authors
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
 
-export const APP_VERSION = "v0.0.0-mock";
+export const APP_VERSION = "v3.0.1-5-g950a83e-dirty";
 export const RUST_PATHS = {
   CONFIG: "/data/adb/meta-hybrid/config.toml",
   MODE_CONFIG: "/data/adb/meta-hybrid/module_mode.conf",
@@ -11,12 +7,4 @@ export const RUST_PATHS = {
   DAEMON_STATE: "/data/adb/meta-hybrid/run/daemon_state.json",
   DAEMON_LOG: "/data/adb/meta-hybrid/daemon.log",
 } as const;
-export const BUILTIN_PARTITIONS = [
-  "system",
-  "vendor",
-  "product",
-  "system_ext",
-  "odm",
-  "oem",
-  "apex",
-] as const;
+export const BUILTIN_PARTITIONS = ["system", "vendor", "product", "system_ext", "odm", "oem", "apex"] as const;
