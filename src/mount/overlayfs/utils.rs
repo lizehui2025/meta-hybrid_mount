3a7a7ca94439881a676b111f267eacfd634bd0d3
Tools-app <localhost.hutao@gmail.com>
2026-02-06 08:06:55 +0800

    refactor: refactor ext4 img mount logic
    No longer dependent on external commands

--- src/mount/overlayfs/utils.rs
+++ src/mount/overlayfs/utils.rs
@@ -1,12 +1,17 @@
 // Copyright 2026 https://github.com/KernelSU-Modules-Repo/meta-overlayfs and https://github.com/bmax121/APatch
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
-use std::{fs, os::unix::fs::PermissionsExt, path::Path, process::Command};
+use std::{fs, os::unix::fs::PermissionsExt, path::Path};
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
-use anyhow::{Context, Result, anyhow};
+use anyhow::{Context, Result};
 #[cfg(any(target_os = "linux", target_os = "android"))]
-use rustix::mount::{UnmountFlags, unmount};
+use loopdev::LoopControl;
+#[cfg(any(target_os = "linux", target_os = "android"))]
+use rustix::{
+    mount::{MountFlags, UnmountFlags, mount, unmount},
+    path::Arg,
+};
 
 pub struct AutoMountExt4 {
     target: String,
@@ -19,8 +24,6 @@ impl AutoMountExt4 {
     where
         P: AsRef<Path>,
     {
-        use rustix::path::Arg;
-
         let path = source.as_ref();
         if !path.exists() {
             println!("Source path does not exist");
@@ -34,7 +37,7 @@ impl AutoMountExt4 {
             }
         }
 
-        mount_ext4(source, target.as_ref())?;
+        mount_ext4(source.as_ref(), target.as_ref())?;
         Ok(Self {
             target: target.as_ref().as_str()?.to_string(),
             auto_umount,
@@ -71,17 +74,35 @@ impl Drop for AutoMountExt4 {
 }
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
-pub fn mount_ext4(source: impl AsRef<Path>, target: impl AsRef<Path>) -> Result<()> {
-    let status = Command::new("mount")
-        .args(["-t", "ext4", "-o", "loop,rw,noatime"])
-        .arg(source.as_ref())
-        .arg(target.as_ref())
-        .status()
-        .context("Failed to execute mount command")?;
+pub fn mount_ext4<P>(source: P, target: P) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    let lc = LoopControl::open().context("Failed to open loop control")?;
+    let ld = lc.next_free().context("Failed to find free loop device")?;
 
-    if !status.success() {
-        return Err(anyhow!("Mount command failed"));
-    }
+    ld.with()
+        .read_only(false)
+        .autoclear(true)
+        .attach(source.as_ref())
+        .context("Failed to attach source to loop device")?;
+
+    let device_path = ld.path().context("Could not get loop device path")?;
+    log::debug!("loop device path: {}", device_path.display());
+
+    mount(
+        &device_path,
+        target.as_ref(),
+        "ext4",
+        MountFlags::NOATIME,
+        Some(c""),
+    )
+    .context(format!(
+        "Failed to mount {} to {}",
+        device_path.display(),
+        target.as_ref().display()
+    ))?;
+
     Ok(())
 }
 
